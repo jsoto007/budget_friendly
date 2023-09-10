4a56dd5ee7e0b43135a1957724859cb7ea050d24
Jose Soto <jsoto7087@gmail.com>
2023-09-09 22:50:20 -0400

    completed front end

--- client/src/app/login.js
+++ client/src/app/login.js
@@ -8,7 +8,6 @@ function Login() {
     password:""
   });
 
-
   function handleChange() {
     const key = e.target.value
     setFormData({
@@ -26,7 +25,7 @@ function Login() {
   }
 
   return ( 
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 db-slate-100 dark:bg-slate-300">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 db-slate-100 dark:bg-slate-200">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
      
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -49,7 +48,7 @@ function Login() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900        shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900        shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 px-2"
                 />
               </div>
             </div>
@@ -60,7 +59,7 @@ function Login() {
                   Password
                 </label>
                 <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  <a href="#" className="font-semibold text-blue-600 hover:text-blue-500">
                     Forgot password?
                   </a>
                 </div>
@@ -74,7 +73,7 @@ function Login() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 px-2"
                 />
               </div>
             </div>
@@ -82,7 +81,7 @@ function Login() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 Sign in
               </button>
