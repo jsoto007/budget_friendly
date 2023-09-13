29efa9b3e3354049c633f2e54203083002796c11
Jose Soto <jsoto7087@gmail.com>
2023-09-13 19:33:27 -0400

    working with no errors

--- client/src/app/signup/page.js
+++ client/src/app/signup/page.js
@@ -41,7 +41,7 @@ export default function Signup() {
                           type="email" 
                           name="email" 
                           id="email" 
-                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" 
+                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@email.com" 
                           required="" 
                         />
                     </div>
