65703885d623d6291c8ab1a2ae62c8b152f1968c
Jose Soto <jsoto7087@gmail.com>
2023-09-17 12:37:15 -0400

    complete sign up and login

--- client/src/app/components/Signup.js
+++ client/src/app/components/Signup.js
@@ -1,96 +1,115 @@
-
-
+import { useState } from 'react'
 
 export default  function Signup( { onChangeLogin } ) {
 
+  const [formData, setFormData] = useState({
+    email: "",
+    password:"",
+    confirmPassword:""
+  });
+
+  function handleChange() {
+    const key = e.target.value
+    setFormData({
+      ...formData, 
+      [key]: e.target.value
+    })
+  }
+
+  function handleChange(e) {
+    const key = e.target.id
+    setFormData({
+      ...formData,
+      [key]: e.target.value
+    })
+  }
+
   return( 
-    <section class="bg-gray-50 dark:bg-gray-700">
-    <div 
-      class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 db-white dark:bg-slate-200">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+ 
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Signup
+      </h2>
+    </div>
+
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" action="#" method="POST">
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              value={formData.value}
+              onChange={handleChange}
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900        shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 px-2"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              Password
+            </label>
+          
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              type="password"
+              autoComplete="current-password"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 px-2"
+            />
+          </div>
+          <label htmlFor="password-confirm" className="block text-sm font-medium leading-6 text-gray-900">
+              Confirm Password
+            </label>
+          <div className="mt-2">
+            <input
+              id="confirmPassword"
+              name="password-confirm"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              type="password"
+              autoComplete="current-password"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 px-2"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-emerald-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >
+            Create Account
+          </button>
+        </div>
+      </form>
+
+      <p className="mt-10 text-center text-sm text-gray-500">
+        Already a member?{' '}
         <a 
           href="#" 
-          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          onClick={onChangeLogin}
         >
-        Brudget Frindly    
+        Click Here to Login!
         </a>
-        <div 
-          class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-slate-600 dark:border-gray-700"
-        >
-            <div 
-              class="p-6 space-y-4 md:space-y-6 sm:p-8"
-            >
-                <h1 
-                  class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
-                >
-                Create an account
-                </h1>
-                <form 
-                  class="space-y-4 md:space-y-6" 
-                  action="#"
-                >
-                    <div>
-                        <label 
-                          for="email" 
-                          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >Your email
-                        </label>
-                        <input 
-                          type="email" 
-                          name="email" 
-                          id="email" 
-                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@email.com" 
-                          required="" 
-                        />
-                    </div>
-                    <div>
-                        <label 
-                          for="password" 
-                          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >Password
-                        </label>
-                        <input 
-                          type="password" 
-                          name="password" 
-                          id="password" 
-                          placeholder="••••••••" 
-                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" 
-                        />
-                    </div>
-                    <div>
-                        <label 
-                          for="confirm-password" 
-                          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >Confirm password
-                        </label>
-                        <input 
-                          type="password" 
-                          name="confirm-password" 
-                          placeholder="••••••••" 
-                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" 
-                        />
-                    </div>
-                    <div 
-                      class="flex items-start"
-                    >
-                    </div>
-                    <button 
-                      type="submit" 
-                      class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                    >Create an account
-                    </button>
-                    <p 
-                      class="text-sm font-light text-gray-500 dark:text-gray-400"
-                    >Already have an account? 
-                    <a
-                      onClick={onChangeLogin} 
-                      href="#" 
-                      class="font-medium text-primary-600 hover:underline dark:text-primary-500 text-blue-600 ml-2"
-                    >Login here</a>
-                    </p>
-                </form>
-            </div>
-        </div>
+      </p>
     </div>
-  </section>
+  </div>
   )
 }
