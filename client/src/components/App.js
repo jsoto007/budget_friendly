61ec1f41f4f042609191187d2b88a9624b67b2c0
Jose Soto <jsoto7087@gmail.com>
2023-10-15 00:23:28 -0400

    add login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,14 @@
 
 
+import Login from "./Login"
+
 export default function App() {
   return (
     <div className="bd-red-700">
       <h1 className="text-3xl font-bold underline text-red-800">
         Hello world!
       </h1>
+      <Login />
     </div>
   )
 }
