b41f41c97ff7942f245cf1550ec22e7a0b6927ff
Jose Soto <jsoto7087@gmail.com>
2023-10-15 00:35:59 -0400

    working with no errors

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,11 @@ import React from "react"
 import AuthConteiner from "./AuthConteiner"
 
 export default function App() {
+
+  
+
+
+
   return (
     <div className="bd-red-700">
       <AuthConteiner />
