75eb59059405906d379723f3870d30b8c970f9fe
Jose Soto <jsoto7087@gmail.com>
2023-10-15 00:44:55 -0400

    working on login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,27 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import AuthConteiner from "./AuthConteiner"
 
 export default function App() {
 
-  
+  const [user, setUser] = useState(false)
 
 
+  useEffect(() => {
+    fetch('/check_session')
+    .then(resp => {
+      if (resp.ok){
+        resp.json().then(user => {
+          setUser(user)
+        })
+      }
+    })
+  }, [])
+
+  if (!user) return <AuthConteiner />
 
   return (
     <div className="bd-red-700">
-      <AuthConteiner />
+      Hello from Home!
     </div>
   )
 }
