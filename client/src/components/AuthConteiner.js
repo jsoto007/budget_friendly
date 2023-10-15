2f248f713e141c789c12e71ca243f0f25f3c3459
Jose Soto <jsoto7087@gmail.com>
2023-10-15 00:35:04 -0400

    user can Signup or create acc

--- client/src/components/AuthConteiner.js
+++ client/src/components/AuthConteiner.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./Login";
 import Signup from "./Signup";
 
 export default function AuthConteiner() {
 
+  const [isLogin, setIsLogin] = useState(true)
 
+  function changeLogintoSignup() {
+    setIsLogin((isLogin) => !isLogin)
+  }
 
   return (
-    <div>
-      <Login />
-      <Signup />
+    <div> 
+      {isLogin? (<Login onChangeLogin={changeLogintoSignup} />):(<Signup onChangeLogin={changeLogintoSignup} />)}
     </div>
   )
 }
