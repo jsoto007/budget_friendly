cda328b9e3db6fb3899c3eb545375df3502e8d15
Kevin Salinas <kevinsalinas155@gmail.com>
2023-10-21 13:00:29 -0400

    remove signup password field

---
+++ client/src/app/context/user.js
@@ -0,0 +1,22 @@
+'use client'
+
+import React from 'react';
+import {useState} from 'react';
+
+
+const UserContext = React.createContext();
+
+function UserProvider({ children } ) {
+
+  const [user, setUser] = useState(null)
+
+
+  return (
+    <UserContext.Provider value={{ user,setUser }}>
+        { children }
+    </UserContext.Provider>
+  )
+
+}
+
+export { UserContext, UserProvider };
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -45,7 +45,7 @@ export default  function Signup( { onChangeLogin } ) {
           </div>
         </div>
 
-        <div>
+        {/* <div>
           <div className="flex items-center justify-between">
             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
               Password
@@ -79,7 +79,7 @@ export default  function Signup( { onChangeLogin } ) {
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 px-2"
             />
           </div>
-        </div>
+        </div> */}
 
         <div>
           <button
