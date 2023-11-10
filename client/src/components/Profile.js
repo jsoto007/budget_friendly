2c310fad245600a90d814d52940091e22ad3510d
Jose Soto <jsoto7087@gmail.com>
2023-11-10 00:21:57 -0500

    working on profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -4,14 +4,10 @@ import React from "react";
 export default function Profile() {
 
   return(
-    <div className="pt-20">
+    <div className="pt-16">
       <div className="sm:grid grid-cols-1">
         <h3 className="ml-1 font-serif font-bold text-neutral-200">Fixed Expenses</h3>
-          <div className="bg-neutral-200 w-auto mx-1 rounded-md">
-            hello from the other side
-          </div>
-        <h3 className="ml-1 font-serif font-bold text-neutral-200">Fixed Expenses</h3>
-          <div className="bg-neutral-200 w-auto mx-1 rounded-md">
+          <div className="bg-neutral-200 w-auto mx-1 rounded-md mb-4">
             hello from the other side
             <ul>
               <li>Rent $1900 Montly</li>
@@ -20,8 +16,26 @@ export default function Profile() {
               <li>Rent $1900 Montly</li>
             </ul>
           </div>
+        <h3 className="ml-1 font-serif font-bold text-neutral-200">Other Expenses</h3>
+          <div className="bg-neutral-200 w-auto mx-1 rounded-md">
+            hello from the other side
+            <ul className="my-6 ml-2">
+              <li><span className="font-bold">Description | </span>Montly Rent</li>
+              <li><span className="font-bold">Frequency | </span>Montly</li>
+              <li><span className="font-bold">Amount | </span>$1990.95</li>
+            </ul>
+            <ul className="my-6 ml-2">
+              <li><span className="font-bold">Description | </span>Montly Rent</li>
+              <li><span className="font-bold">Frequency | </span>Montly</li>
+              <li><span className="font-bold">Amount | </span>$1990.95</li>
+            </ul>
+            <ul className="my-6 ml-2">
+              <li><span className="font-bold">Description | </span>Montly Rent</li>
+              <li><span className="font-bold">Frequency | </span>Montly</li>
+              <li><span className="font-bold">Amount | </span>$1990.95</li>
+            </ul>
+          </div>
       </div>
-      Hello
     </div>
   )
 }
