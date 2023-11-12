963df8dccc72279da93d924065f3f946eafc25ec
Kevin Salinas <kevinsalinas155@gmail.com>
2023-11-12 01:15:38 -0500

    create table classes first

--- server/models.py
+++ server/models.py
@@ -1,13 +1,18 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.ext.associationproxy import association_proxy
+
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String)
+    email = db.Column(db.String, unique=True, nullable=False)
     name = db.Column(db.String)
+
+    image = db.Column(db.String)
+    bio = db.Column(db.String)
 
     _password_hash = db.Column(db.String)
 
@@ -23,5 +28,25 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
     
+
+class Expense(db.Model, SerializerMixin):
+    __tablename__ = 'expenses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    expense = db.Column(db.Float)
+    description = db.Column(db.String)
+    category = db.Column(db.String)
+    recurrence = db.Column(db.String)
+
+
+class Transaction(db.Model, SerializerMixin):
+    __tablename__ = 'transactions'
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    expense_id = db.Column(db.Integer, db.ForeignKey('expenses.id'))
+
+    date = db.Column(db.String)
 
 
