90b9011c11d984f2fc41e53ffb43b57ee2fdd56e
Kevin Salinas <kevinsalinas155@gmail.com>
2023-11-21 19:06:15 -0500

    update seed file 2 users

--- server/seed.py
+++ server/seed.py
@@ -1,22 +1,38 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app import app
-from models import db, User
+from models import db, User, Expense, Transaction
 
 with app.app_context():
 
     print("Delete data...")
     User.query.delete()
-
+    Expense.query.delete()
+    Transaction.query.delete()
 
     print("Creating user...")
-    user1 = User(email = "test@gmail.com", name = "Test Name", image = "https://depositphotos.com/vector/user-profile-icon-137014128.html", bio = "this is a temp bio")
+    user1 = User(
+        email = "test@gmail.com", 
+        name = "Test Name", 
+        image = "https://depositphotos.com/vector/user-profile-icon-137014128.html", 
+        bio = "this is a temp bio"
+    )
     user1.password_hash = "test"
+
+    user2 = User(
+        email = "test2@gmail.com", 
+        name = "Test2 Name", 
+        image = "https://depositphotos.com/vector/user-profile-icon-137014128.html", 
+        bio = "this is a temp bio as well"
+    )
+    user2.password_hash = "test2"
+
+    users = [user1, user2]
 
     # this will let you see the password not hashed (line 17)
     # user1._password_hash = "test" 
 
-    db.session.add(user1)
+    db.session.add_all(users)
     db.session.commit()
 
     print("Seeding done!")
