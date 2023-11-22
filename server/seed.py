316788754ed63192fda16d3bd9d78dce620d2e02
Kevin Salinas <kevinsalinas155@gmail.com>
2023-11-21 19:32:33 -0500

    add seed data for expense and transaction

--- server/seed.py
+++ server/seed.py
@@ -10,7 +10,7 @@ with app.app_context():
     Expense.query.delete()
     Transaction.query.delete()
 
-    print("Creating user...")
+    print("Creating User...")
     user1 = User(
         email = "test@gmail.com", 
         name = "Test Name", 
@@ -29,10 +29,53 @@ with app.app_context():
 
     users = [user1, user2]
 
+    print("Creating Expense...")
+    exp1 = Expense(
+        expense_incurred = '20.00',
+        description = "Haircut",
+        category = "Self-Groom",
+        recurrence = "Monthly"
+    )
+
+    exp2 = Expense(
+        expense_incurred = '100.00',
+        description = "Jacket",
+        category = "Clothing",
+        recurrence = "One-Time"
+    )
+
+    expenses = [exp1, exp2]
+
+    print("Creating Transaction...")
+    
+    transaction1 = Transaction(
+        user = user1, 
+        expense = exp2, 
+        date = "11/20/23",
+    )
+
+    transaction2 = Transaction(
+        user = user2, 
+        expense = exp2, 
+        date = "11/10/23",
+    )
+
+    transaction3 = Transaction(
+        user = user2, 
+        expense = exp1, 
+        date = "11/14/23",
+    )
+
+    transactions = [transaction1, transaction2, transaction3]
+
+
     # this will let you see the password not hashed (line 17)
     # user1._password_hash = "test" 
 
     db.session.add_all(users)
+    db.session.add_all(expenses)
+    db.session.add_all(transactions)
+
     db.session.commit()
 
     print("Seeding done!")
