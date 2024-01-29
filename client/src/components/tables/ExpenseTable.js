34bc21f9385e64df25764c13cb5c8a2cef618e16
Jose Soto <jsoto7087@gmail.com>
2024-01-28 19:42:13 -0500

    adds form data dynamically"

--- client/src/components/tables/ExpenseTable.js
+++ client/src/components/tables/ExpenseTable.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../../context/user";
 
 export default function ExpenseTable() {
+
+  const { user } = useContext(UserContext)
+
+  const userExpense = user.expenses
+  console.log("userDATA", userExpense[0].category)
 
   return (
 
@@ -19,6 +25,9 @@ export default function ExpenseTable() {
                         Price
                     </th>
                     <th scope="col" className="px-6 py-3">
+                        Recurrence
+                    </th>
+                    <th scope="col" className="px-6 py-3">
                         Description
                     </th>
                     <th scope="col" className="px-6 py-3">
@@ -27,57 +36,32 @@ export default function ExpenseTable() {
                 </tr>
             </thead>
             <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              {userExpense.map((expense) => {
+                return (
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Apple MacBook Pro 17"
+                        {expense.description}
                     </th>
                     <td className="px-6 py-4">
-                        Silver
+                        {expense.id} 22-2024
                     </td>
                     <td className="px-6 py-4">
-                        Laptop
+                        ${expense.expense_incurred}
                     </td>
                     <td className="px-6 py-4">
-                        $2999
+                        {expense.recurrence}
+                    </td>
+                    <td className="px-6 py-4">
+                        {expense.description}
                     </td>
                     <td className="px-6 py-4 text-right">
                         <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                     </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Microsoft Surface Pro
-                    </th>
-                    <td className="px-6 py-4">
-                        White
-                    </td>
-                    <td className="px-6 py-4">
-                        Laptop PC
-                    </td>
-                    <td className="px-6 py-4">
-                        $1999
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    </td>
-                </tr>
-                <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Magic Mouse 2
-                    </th>
-                    <td className="px-6 py-4">
-                        Black
-                    </td>
-                    <td className="px-6 py-4">
-                        Accessories
-                    </td>
-                    <td className="px-6 py-4">
-                        $99
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    </td>
-                </tr>
+
+                )
+              })}
+            
             </tbody>
         </table>
     </div>
